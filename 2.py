1425ef19204e238c5f74e571bfaa60a290681731
s00ndie <s00ndie@icloud.com>
2024-09-17 10:51:15 +0200

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,13 @@
+import re
+def first_word(text):
+    ko = "'"
+    match = re.search(r"\w+'\w*|\w+", text)
+    return match.group()
+
+assert first_word("Hello world") == "Hello", 'Test1'
+assert first_word("greetings, friends") == "greetings", 'Test2'
+assert first_word("don't touch it") == "don't", 'Test3'
+assert first_word(".., and so on ...") == "and", 'Test4'
+assert first_word("hi") == "hi", 'Test5'
+assert first_word("Hello.World") == "Hello", 'Test6'
+print('OK')
